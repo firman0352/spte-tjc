31b453af44aab64008c0d1abdecee27b39a4daa2
Firman <firmansyah0352@gmail.com>
2023-08-20 21:04:47 +0700

    add jabatan

---
+++ app/Http/Controllers/JabatanController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jabatan;
+use Illuminate\Http\Request;
+
+class JabatanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $jabatan = Jabatan::all();
+
+        return view('jabatan.index', compact('jabatan'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('jabatan.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate(['jabatan' => 'required']);
+
+        Jabatan::create($request->all());
+
+        return redirect()->route('jabatan.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Jabatan $jabatan)
+    {
+        return view('jabatan.edit', compact('jabatan'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Jabatan $jabatan)
+    {
+        $request->validate(['jabatan' => 'required']);
+
+        $jabatan->update($request->all());
+
+        return redirect()->route('jabatan.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Jabatan $jabatan)
+    {
+        $jabatan->delete();
+
+        return redirect()->route('jabatan.index');
+    }
+}
---
+++ resources/views/jabatan/create.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Add Jabatan') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="overflow-hidden overflow-x-auto border-b border-gray-200 bg-white p-6">
+                    <form action="{{ route('jabatan.store') }}" method="POST">
+                        @csrf
+                        
+
+                        <div>
+                            <x-input-label for="jabatan" value="Jabatan" />
+                            <x-text-input id="jabatan" name="jabatan" value="{{ old('jabatan') }}" type="text" class="block mt-1 w-full" />
+                            <x-input-error :messages="$errors->get('jabatan')" class="mt-2" />
+                        </div>
+
+                        <div class="mt-4">
+                            <x-primary-button>
+                                Save
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
