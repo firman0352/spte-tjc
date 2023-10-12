5b1b011d8c0d66c5e33d34a4aaabddc1478eab1a
farizan17 <muhammadfarizankholis@gmail.com>
2023-10-12 09:57:42 +0700

    update edit profil

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,25 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-// function data() {
-//     function getIsMinimizeSidebarFromLocalStorage() {
-//         if (window.localStorage.getItem("minimizeSidebar")) {
-//           return JSON.parse(window.localStorage.getItem("minimizeSidebar"));
-//         }
-    
-//         return false;
-//       }
-    
-//     function setIsMinimizeSidebarToLocalStorage(value) {
-//         window.localStorage.setItem("minimizeSidebar", value);
-//       }
-//       return {  
-    
-//         isMinimizeSidebar: getIsMinimizeSidebarFromLocalStorage(),
-//         toggleMinimizeSidebar() {
-//           this.isMinimizeSidebar = !this.isMinimizeSidebar;
-//           setIsMinimizeSidebarToLocalStorage(this.isMinimizeSidebar);
-//         },
-//         };
-//     }
--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,7 @@ Route::middleware(['auth', 'verified','PreventBackHistory'])->group(function () 
 
 Route::middleware('auth','PreventBackHistory')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile/change-password', [ProfileController::class, 'changePassword'])->name('profile.edit.password');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
